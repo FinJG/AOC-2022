15544ab39b5efb3d90f20a0cc68e06cb4d761b0e
Beelxebub <90899751+FinJG@users.noreply.github.com>
2022-12-03 21:47:00 +0000

    added annotation

--- day1/day1 - part1.py
+++ day1/day1 - part1.py
@@ -1,12 +1,15 @@
 lis = []
 totals = []
 
-with open("input.txt") as i:
-    for loop in i.readlines():
-        if len(loop) > 1:
-            lis.append(int(loop))
-        else:
-            totals.append(sum(lis))
-            lis = []
 
-print sorted(totals)
+with open("input.txt") as f:
+      
+    for loop in f.readlines():        # loops through the file and adds 
+        if len(loop) > 1:             # every line that isnt empty to a list
+            lis.append(int(loop))
+              
+        else:                         # if a empty line is found the sum of                             
+            totals.append(sum(lis))   # the list is added to the "totals" list
+            lis = []                  # and then original list is cleared
+
+print(sorted(totals)[-1])   #sorts the list and prints the last index
