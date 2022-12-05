7b550ca888600e0819959070b764c20000d36cab
FinJG <finlay.griffiths03@gmail.com>
2022-12-05 20:42:06 +0000

    more streamlining

--- day3/day3 - part1.py
+++ day3/day3 - part1.py
@@ -1,14 +1,11 @@
-import string
-
-alpha = list(string.ascii_letters)
 lis = []
 
 with open("input.txt") as file:
-    for line in [[char for char in line][:-1] for line in file]:
-        half = int(len(line)/2)
+    for line in file:
+        half = len(line) // 2
         for index in line[:half]:
             if index in line[half:]:
                 lis.append(index)
                 break
 
-print(sum([alpha.index(i)+1 for i in lis]))
+print(sum([ord(i) - 96 if i.islower() else ord(i) - 38 for i in lis]))
