2342c4e0d69fa12fefeddf99f36f55ebe1dba55f
FinJG <finlay.griffiths03@gmail.com>
2022-12-05 17:18:57 +0000

    Added connotation

--- day2/day2 - part1.py
+++ day2/day2 - part1.py
@@ -2,9 +2,9 @@ dic = {
     "A X\n":4,
     "A Y\n":8,
     "A Z\n":3,
-    "B X\n":1,
-    "B Y\n":5,
-    "B Z\n":9,
+    "B X\n":1,  # this dictionary contains every pissible
+    "B Y\n":5,  # hand that can be played and the
+    "B Z\n":9,  # corrosponding points that they give
     "C X\n":7,
     "C Y\n":2,
     "C Z\n":6,
@@ -13,6 +13,6 @@ dic = {
 
 score = 0
 with open("input.txt") as f:
-    for line in f.readlines():
-        score += dic[line]       
+    for line in f.readlines(): # loops through each line in the file
+        score += dic[line] # adds the score of each hand to a variable
 print(score)
