45a2305a7809029f9d5156fdfbe80fe1809d66b6
FinJG <finlay.griffiths03@gmail.com>
2022-12-06 18:13:49 +0000

    updated code to work on the unedited input
    fixed a problem where the program would only work if a new line was added at the end of the input

--- day2/day2 - part1.py
+++ day2/day2 - part1.py
@@ -1,18 +1,17 @@
 dic = {
-    "A X\n":4,
-    "A Y\n":8,
-    "A Z\n":3,
-    "B X\n":1,  # this dictionary contains every pissible
-    "B Y\n":5,  # hand that can be played and the
-    "B Z\n":9,  # corrosponding points that they give
-    "C X\n":7,
-    "C Y\n":2,
-    "C Z\n":6,
+    "A X":4,
+    "A Y":8,
+    "A Z":3,
+    "B X":1,  # this dictionary contains every pissible
+    "B Y":5,  # hand that can be played and the
+    "B Z":9,  # corrosponding points that they give
+    "C X":7,
+    "C Y":2,
+    "C Z":6,
 }
 
-
 score = 0
-with open("input.txt") as f:
-    for line in f.readlines(): # loops through each line in the file
+with open("input.txt") as i:
+    for line in i.read().strip().splitlines(): # loops through each line in the file
         score += dic[line] # adds the score of each hand to a variable
 print(score)
