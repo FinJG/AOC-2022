d1cd10c4b1e99a7b16565071129351ee6fd59a76
FinJG <finlay.griffiths03@gmail.com>
2022-12-06 18:31:19 +0000

    updated code to work with original input

--- day4/day4 - part2.py
+++ day4/day4 - part2.py
@@ -1,7 +1,7 @@
 count = 0
 
 with open("input.txt") as f:
-    f = [[i.split("-") for i in line.split(",")] for line in f]
+    f = [[i.split("-") for i in line.split(",")] for line in f.read().strip().splitlines()]
     
     for loop in range(len(f)):
         sett1 = set(range(int(f[loop][0][0]), int(f[loop][0][1])+1))
@@ -13,4 +13,3 @@ with open("input.txt") as f:
                 break
 
 print(count)
-
