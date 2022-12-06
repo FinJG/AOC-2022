708083b9fa37150debba2354aa91026057e25035
FinJG <finlay.griffiths03@gmail.com>
2022-12-06 12:31:08 +0000

    improved code based on suggestions

--- day3/day3 - part2.py
+++ day3/day3 - part2.py
@@ -1,18 +1,14 @@
-import string
-
-lis = []
-alpha = list(string.ascii_letters)
+score = 0
 
 with open("input.txt") as file:
-    file = [[char for char in line][:-1] for line in file]
-    for line in file:
-        for index in alpha:
-            if not file.index(line) % 3:
-                backpack = file[file.index(line)-3:file.index(line)]
-                if len(backpack) > 0:
-                    if index in backpack[0] and index in backpack[1] and index in backpack[2]:
-                        lis.append(index)
+    f = file.read().strip().splitlines()
+    for line in [f[i:i + 3] for i in range(0, len(f), 3)]:
+        for letter in line[0]:
+            if letter in line[1] and letter in line[2]:
+                if letter.islower():
+                    score += ord(letter) - 96
+                else:
+                    score += ord(letter) - 38
+                break
 
-
-print(len(lis))
-print(sum([alpha.index(i)+1 for i in lis]))
+print(score)
