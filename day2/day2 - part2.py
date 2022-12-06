6b51c76e2948fa2f1b603d87269dc19312255912
FinJG <finlay.griffiths03@gmail.com>
2022-12-06 18:17:23 +0000

    updated code to work on original input

--- day2/day2 - part2.py
+++ day2/day2 - part2.py
@@ -1,17 +1,17 @@
 dic = {
-    "A X\n":3,
-    "A Y\n":4,
-    "A Z\n":8,
-    "B X\n":1,  # this dictionary contains all of the possible
-    "B Y\n":5,  # combinations of hands that could be thrown
-    "B Z\n":9,  # and the corrosponding points that they give
-    "C X\n":2,
-    "C Y\n":6,  # the only change in part2 of this challenge
-    "C Z\n":7,  # are the values used in the dictionary 
+    "A X":3,
+    "A Y":4,
+    "A Z":8,
+    "B X":1,  # this dictionary contains all of the possible
+    "B Y":5,  # combinations of hands that could be thrown
+    "B Z":9,  # and the corrosponding points that they give
+    "C X":2,
+    "C Y":6,  # the only change in part2 of this challenge
+    "C Z":7,  # are the values used in the dictionary 
 }
 
 score = 0
-with open("input.txt") as f:
-    for line in i.readlines(): # loops through each line in the file
-        score += dic[line] # adds each hands corrosponding value to a variable
+with open("input.txt") as i:
+    for line in i.read().strip().splitlines(): # loops through each line in the file
+        score += dic[line] # adds the score of each hand to a variable
 print(score)
