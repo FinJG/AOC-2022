27274f07cabb0c90c428f2601d7f15c1976063d5
FinJG <finlay.griffiths03@gmail.com>
2022-12-10 17:22:31 +0000

    fixing my mistakes

--- day6/day6 - part1.py
+++
@@ -1,7 +0,0 @@
-with open("input.txt") as file:
-    f = file.readlines()[0]
-    for i in range(len(f)): 
-        hold = f[i:i+4]
-        if len(hold) == len(set(hold)):
-            print(i+4)
-            break
