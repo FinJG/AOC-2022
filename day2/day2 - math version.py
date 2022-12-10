d5bb21d2ed8802f054cf1e1272aa0decb554fd95
FinJG <finlay.griffiths03@gmail.com>
2022-12-10 17:21:56 +0000

    fixing my mistakes

--- day2/day2 - math version.py
+++
@@ -1,5 +0,0 @@
-score = 0
-for line in open("input.txt").read().strip().splitlines():
-    myindex = ord(line[2]) - 88
-    score += (((myindex - (ord(line[0]) - 66)) % 3)* 3) + myindex + 1
-print(score)
--- day2/day2 - part1 one line.py
+++
@@ -1,1 +0,0 @@
-print(sum([(ord(line[2]) - 154 - ord(line[0])) % 3 * 3 + ord(line[2]) - 87 for line in open("input.txt")]))
